35647a7aca61142961f1dfa61798a6521776af7a
Nathan Conrad <pigrew@gmail.com>
2018-05-27 18:40:31 -0400

    Create remarks merging script

--- FloatingFirmware/disasm.sh
+++ FloatingFirmware/disasm.sh
@@ -1,1 +1,4 @@
-../Dasmx.exe -c6303 86float.bin
+#!/usr/bin/bash
+../../Dasmx.exe 86float.bin
+
+./mergeRemarks.py 86float.lst  86float.rem > 86float_rem.lst
---
+++ FloatingFirmware/mergeRemarks.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+# This script merges comments into the output of dasmx
+
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+	
+def getNextRemark(rem_fp):
+	while (True):
+		rem_line = rem_fp.readline();
+		# File complete
+		if(not rem_line):
+			return None
+			
+		rem_line = rem_line.rstrip()
+		# Check for empty remark line
+		if (rem_line.isspace() or not rem_line):
+			continue
+			
+		rem_parts = rem_line.split(None,maxsplit=1)
+		rem_line_num = int(rem_parts[0], 16)
+		#print("XY ```%s''' <<<%s>>>" %(rem_line_num, rem_parts[1]))
+		return (rem_line_num, rem_parts[1]);
+	
+
+eprint("Merging Remarks into source file")
+
+asmFilename = sys.argv[1]
+remFilename = sys.argv[2]
+
+eprint("remFilename is %s" % (remFilename))
+
+rem_fp = open(remFilename, 'r')
+asm_fp = open(asmFilename, 'r')
+
+rem = getNextRemark(rem_fp)
+exit
+for asm_line in asm_fp:
+	# Read a line
+	asm_line = asm_line.rstrip()
+	# Is it a code line?
+	if(len(asm_line) > 9 and asm_line[4] == ' ' and asm_line[5] == ':' and asm_line[6] == ' '):
+		asm_parts = asm_line.split(None,2)
+		asm_line_num = int(asm_parts[0],16)
+		while(rem and rem[0] < asm_line_num):
+			print("R ; " + rem[1])
+			rem = getNextRemark(rem_fp)
+			
+		if(rem and rem[0] == asm_line_num):
+			print("M "+ asm_line + "\t; " + rem[1])
+			rem = getNextRemark(rem_fp)
+		while(rem and rem[0] == asm_line_num):
+			print("N \t; " + rem[1])
+			rem = getNextRemark(rem_fp)
+		print("C %s" % (asm_line))
+	else:
+		print("N %s" % (asm_line))
+	
+asm_fp.close()
+rem_fp.close()
